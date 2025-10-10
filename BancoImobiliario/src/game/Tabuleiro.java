6948ee03277a11bcfd9ee4073fa544ddc97d2cc5
José Carlos <jcsneto16@gmail.com>
2025-10-09 23:00:26 -0300

    Create class tabuleiro

---
+++ BancoImobiliario/src/game/Tabuleiro.java
@@ -0,0 +1,62 @@
+package game;
+
+import java.util.ArrayList;
+
+class Tabuleiro {
+    private ArrayList<Object> casas;
+
+    public Tabuleiro() {
+        casas = new ArrayList<>();
+    }
+    
+    private void inicializarCasas() {
+        casas.add("PARTIDA");
+
+        casas.add(new CardPropriedade("Leblon", 100, 50, 50, new int[]{6, 30, 90, 270, 400, 500}));
+//        casas.add(new CardSorteReves("SORTE/REVÉS", 0));
+        casas.add(new CardPropriedade("Av. Presidente Vargas", 60, 50, 50, new int[]{2, 10, 30, 90, 160, 250}));
+        casas.add(new CardPropriedade("Av. Nossa Sra. de Copacabana", 60, 50, 250, new int[]{4, 20, 60, 180, 320, 450}));
+//        casas.add(new CardCompanhia("Companhia Ferroviária", 200, 25));
+//        casas.add(new CardPropriedade("Av. Brigadeiro Faria Lima", 120, new int[]{8, 40, 100, 300, 450, 600}, 50, 250));
+//        casas.add(new CardCompanhia("Companhia de Viação", 150, 25));
+//        casas.add(new CardPropriedade("Av. Rebouças", 120, new int[]{8, 40, 100, 300, 450, 600}, 50, 250));
+//        casas.add(new CardPropriedade("Av. 9 de Julho", 220, new int[]{10, 50, 150, 450, 625, 750}, 50, 250));
+//
+//        casas.add("PRISÃO");
+//
+//        casas.add(new CardPropriedade("Av. Europa", 160, new int[]{12, 60, 180, 500, 700, 900}, 100, 500));
+//        casas.add(new CardSorteReves("SORTE/REVÉS", 0));
+//        casas.add(new CardPropriedade("Rua Augusta", 180, new int[]{14, 70, 200, 550, 750, 950}, 100, 500));
+//        casas.add(new CardPropriedade("Av. Pacaembu", 180, new int[]{14, 70, 200, 550, 750, 950}, 100, 500));
+//        casas.add(new CardCompanhia("Companhia de Táxi", 150, 25));
+//        casas.add(new CardSorteReves("SORTE/REVÉS", 0));
+//        casas.add(new CardPropriedade("Interlagos", 200, new int[]{16, 80, 220, 600, 800, 1000}, 100, 500));
+//        casas.add("Lucros ou Dividendos");
+//        casas.add(new CardPropriedade("Morumbi", 220, new int[]{18, 90, 250, 700, 875, 1050}, 100, 500));
+//
+//        casas.add("PARADA LIVRE");
+//
+//        casas.add(new CardPropriedade("Flamengo", 240, new int[]{20, 100, 300, 750, 925, 1100}, 150, 750));
+//        casas.add(new CardSorteReves("SORTE/REVÉS", 0));
+//        casas.add(new CardPropriedade("Botafogo", 260, new int[]{22, 110, 330, 800, 975, 1150}, 150, 750));
+//        casas.add("Imposto de Renda");
+//        casas.add(new CardCompanhia("Companhia de Navegação", 200, 25));
+//        casas.add(new CardPropriedade("Av. Brasil", 280, new int[]{24, 120, 360, 850, 1025, 1200}, 150, 750));
+//        casas.add(new CardSorteReves("SORTE/REVÉS", 0));
+//        casas.add(new CardPropriedade("Av. Paulista", 300, new int[]{26, 130, 390, 900, 1100, 1275}, 200, 1000));
+//        casas.add(new CardPropriedade("Jardim Europa", 320, new int[]{28, 150, 450, 1000, 1200, 1400}, 200, 1000));
+//
+//        casas.add("VÁ PARA A PRISÃO");
+//
+//        casas.add(new CardPropriedade("Copacabana", 340, new int[]{30, 160, 480, 1100, 1275, 1400}, 200, 1000));
+//        casas.add(new CardCompanhia("Companhia de Aviação", 200, 25));
+//        casas.add(new CardPropriedade("Av. Vieira Souto", 350, new int[]{35, 175, 500, 1100, 1300, 1500}, 200, 1000));
+//        casas.add(new CardPropriedade("Av. Atlântica", 350, new int[]{35, 175, 500, 1100, 1300, 1500}, 200, 1000));
+//        casas.add(new CardCompanhia("Companhia de Táxi Aéreo", 200, 25));
+//        casas.add(new CardPropriedade("Ipanema", 400, new int[]{50, 200, 600, 1400, 1700, 2000}, 200, 1000));
+//        casas.add(new CardSorteReves("SORTE/REVÉS", 0));
+//        casas.add(new CardPropriedade("Jardim Paulista", 450, new int[]{55, 250, 750, 1500, 1750, 2100}, 250, 1250));
+//        casas.add(new CardPropriedade("Brooklin", 500, new int[]{60, 300, 900, 1600, 1850, 2200}, 250, 1250));
+    }
+    
+}
