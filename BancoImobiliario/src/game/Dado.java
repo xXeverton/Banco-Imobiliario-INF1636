b2c7464715ca386a01b67013c709ff63ef437de3
José Carlos <jcsneto16@gmail.com>
2025-10-09 22:39:06 -0300

    Create class Dado

---
+++ BancoImobiliario/src/game/Dado.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+class Dado {
+	private Random random;
+	
+	public Dado() {
+		this.random = new Random();
+	}
+	
+	public ArrayList<Integer> lancarDados() {
+        ArrayList<Integer> valores = new ArrayList<>();
+        valores.add(random.nextInt(6) + 1);
+        valores.add(random.nextInt(6) + 1);
+        return valores;
+    }
+}
